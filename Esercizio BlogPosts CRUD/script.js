2f12ce485aa7caf47f03d59fb2ac96b95d5d97ff
emanueledistaso <distasoemanuele@gmail.com>
2023-12-05 20:40:45 +0100

    commit to sync devices

--- Esercizio BlogPosts CRUD/script.js
+++ Esercizio BlogPosts CRUD/script.js
@@ -1,92 +1,115 @@
-// Funzione iniziale che si attiva quando il doc
-// HTML è completamente caricato
+//CODICE SIMONE SU CRUD PRESO DAL SUO GITHUB
 
-/* DOMContentLoaded è un evento 
-che si verifica quando il doc HTML è stato
-completamente analizzato e caricato, ma senza
-attendere che tutti gli elementi esterni (tipo img)
-siano stati scaricati.
-*/
-
+// Funzione principale che si attiva quando il documento HTML è completamente caricato
 document.addEventListener("DOMContentLoaded", function () {
-	// Puntatori
+	// Ottenere riferimenti agli elementi del DOM
 	const postsContainer = document.getElementById("posts-container");
 	const addPostForm = document.getElementById("add-post-form");
 
-
-	// JSON Iniziale
+	// JSON iniziale contenente i dati dei post
 	let postsData = {
 		posts: [
 			{
 				id: 1,
-				autore: "Andrea",
-				titolo: "Forza Napoli",
-				testo: "Ma che me ne frega, odio il calcio",
+				autore: "Mario Rossi",
+				titolo: "Il mio primo post",
+				testo: "Questo è il contenuto del mio primo post.",
 			},
 			{
 				id: 2,
-				autore: "Vittorio",
-				titolo: "Basta js",
-				testo: "Passo al backend, odio Simone",
+				autore: "Anna Bianchi",
+				titolo: "Viaggio estivo",
+				testo: "Racconto delle mie avventure durante le vacanze estive.",
 			},
 			{
 				id: 3,
-				autore: "Luca",
-				titolo: "Prossimo corso?",
-				testo: "Il docente lo faccio io",
+				autore: "Luigi Verdi",
+				titolo: "Pensieri notturni",
+				testo: "Riflessioni sulla vita e la filosofia.",
 			},
 		],
 	};
 
-	// Funzione per visualizzare i primi 3 post nella nostra pagina
+	// Funzione per visualizzare i post nella pagina HTML
 	function showPosts(posts) {
-    // Clear del container 
+		// Pulisce il contenuto del container prima di visualizzare i post aggiornati
 		postsContainer.innerHTML = "";
 		posts.forEach((post) => {
-			// Creo un nuovo elemento div per ogni post
+			// Crea un nuovo elemento div per ogni post
 			const postDiv = document.createElement("div");
-			postDiv.classList.add("singolo-post");
+			postDiv.classList.add("singolo-post"); // Aggiunge una classe CSS al div del post
 			postDiv.innerHTML = `
-    <h3>${post.titolo}</h3>
-    <p><strong>Autore:</strong> ${post.autore}</p>
-    <p>${post.testo}</p>
-    <button>Rimuovi</button>
-    <button>Modifica</button>
-    `;
-
-			// Aggiungo il div del post nel container
+        <h3>${post.titolo}</h3>
+        <p><strong>Autore:</strong> ${post.autore}</p>
+        <p>${post.testo}</p>
+        <button onclick="removePost(${post.id})" class="remove-btn">Rimuovi</button>
+        <button onclick="editPost(${post.id})" class="edit-btn">Modifica</button>
+        <div id="edit-form-${post.id}" style="display: none;">
+          <h4>Modifica Post</h4>
+          <form onsubmit="saveEditedPost(event, ${post.id})">
+            <label for="editedAutore">Nuovo Autore:</label>
+            <input type="text" id="editedAutore" required value="${post.autore}">
+            
+            <label for="editedTitolo">Nuovo Titolo:</label>
+            <input type="text" id="editedTitolo" required value="${post.titolo}">
+            
+            <label for="editedTesto">Nuovo Testo:</label>
+            <textarea id="editedTesto" required>${post.testo}</textarea>
+            
+            <button type="submit">Salva Modifiche</button>
+            <button type="button" onclick="cancelEdit(${post.id})">Annulla</button>
+          </form>
+        </div>
+      `;
+			// Aggiunge il div del post al container dei post
 			postsContainer.appendChild(postDiv);
 		});
+	}
+
+	// Funzione per rimuovere un post
+	window.removePost = function (postId) {
+		// Filtra i post rimuovendo il post con l'ID corrispondente
+		postsData.posts = postsData.posts.filter((post) => post.id !== postId);
+		// Aggiorna la visualizzazione dei post
+		showPosts(postsData.posts);
 	};
 
-  // Aggiunta di un nuovo post dal form
-  addPostForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+	// Funzione per aprire il modulo di modifica
+	window.editPost = function (postId) {
+		const editForm = document.getElementById(`edit-form-${postId}`);
+		editForm.style.display = "block";
+	};
 
-    // Puntatori
-    const autoreInput = document.getElementById("autore");
-    const titoloInput = document.getElementById("titolo");
-    const testoInput = document.getElementById("testo");
+	// Funzione per chiudere il modulo di modifica
+	window.cancelEdit = function (postId) {
+		const editForm = document.getElementById(`edit-form-${postId}`);
+		editForm.style.display = "none";
+	};
 
-    // Creo un nuovo post con i valori che prendo dal form
-    const newPost = {
-      id: Date.now(), // timestamp 
-      autore: autoreInput.value,
-      titolo: titoloInput.value,
-      testo: testoInput.value,
-    };
+	// Funzione per salvare le modifiche
+	window.saveEditedPost = function (event, postId) {
+		event.preventDefault();
 
-    // Aggiungiamo il nuovo post all'array dei post!
-    postsData.posts.push(newPost);
+		// Ottenere riferimenti agli elementi del DOM all'interno del modulo di modifica
+		const editedAutoreInput = document.getElementById("editedAutore");
+		const editedTitoloInput = document.getElementById("editedTitolo");
+		const editedTestoInput = document.getElementById("editedTesto");
 
-    // Pulisco i dati di input dopo l'invio
-    autoreInput.value = "";
-    titoloInput.value = "";
-    testoInput.value = "";
+		// Trova il post nel tuo array di post
+		const editedPost = postsData.posts.find((post) => post.id === postId);
 
-    showPosts(postsData.posts);
-  })
+		// Aggiorna i valori del post con i nuovi valori inseriti nel modulo di modifica
+		editedPost.autore = editedAutoreInput.value;
+		editedPost.titolo = editedTitoloInput.value;
+		editedPost.testo = editedTestoInput.value;
 
+		// Chiudi il modulo di modifica
+		cancelEdit(postId);
 
-  showPosts(postsData.posts);
+		// Aggiorna la visualizzazione dei post
+		showPosts(postsData.posts);
+	};
+
+	// Carica i post iniziali al caricamento della pagina
+	showPosts(postsData.posts);
 });
